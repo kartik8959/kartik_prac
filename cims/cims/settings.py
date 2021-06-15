319a1765472c4f6731eaef49ac990cd284ceed2a
kartik jodhani <77157222+kartik8959@users.noreply.github.com>
2021-06-15 18:52:16 +0530

    Update settings.py

--- cims/cims/settings.py
+++ cims/cims/settings.py
@@ -151,7 +151,7 @@ STATICFILES_DIRS=[
 EMAIL_HOST="smtp.gmail.com"
 EMAIL_PORT="587"
 EMAIL_USE_TLS=True
-EMAIL_HOST_USER="pratapmukesh007@gmail.com"
+EMAIL_HOST_USER=""
 EMAIL_HOST_PASSWORD=""
 
 # LOGIN_URL = 'login'
